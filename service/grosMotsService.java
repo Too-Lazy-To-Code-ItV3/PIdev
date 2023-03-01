3955ed62b9ee8de7b00b2df3184dded3423385e1
nourchawebi <121269804+nourchawebi@users.noreply.github.com>
2023-03-01 17:32:08 +0100

    Add files via upload

---
+++ service/grosMotsService.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import interfaces.grosMotsInterface;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import models.Categorie;
+import models.grosMots;
+import models.offreTravail;
+import util.MaConnexion;
+
+/**
+ *
+ * @author nour2
+ */
+public class grosMotsService implements  grosMotsInterface {
+     Connection cnx = MaConnexion.getInstance().getCnx();
+        //******************** suppression*********************************************
+
+    public void Supprimer(grosMots o) {
+        try {
+            Statement st = cnx.createStatement();
+            String req = "DELETE FROM `grosmots2` WHERE idMot= " + o.getIdmot() + "";
+            st.executeUpdate(req);
+            System.out.println("gros mot supprimer avec succès");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }}
+    //******************** affichage*********************************************
+         public List<grosMots> fetchgrosmots() {
+              List<grosMots> grosmots = new ArrayList<>();
+         try {
+            
+             String req = "SELECT * FROM `grosmots2` ";
+             Statement st = cnx.createStatement();
+             ResultSet rs = st.executeQuery(req);
+             while (rs.next()) {
+                 grosMots grosmot= new grosMots();
+                 grosmot.setIdmot(rs.getInt(1));
+                 grosmot.setMot(rs.getString(2));
+                 grosmots.add(grosmot);
+             } 
+         } catch (SQLException ex) {
+             Logger.getLogger(grosMotsService.class.getName()).log(Level.SEVERE, null, ex);
+         }
+return  grosmots;
+    }
+    public List<String> fetchgrosmotsString() {
+              List<String> grosmots = new ArrayList<>();
+         try {
+            
+             String req = "SELECT * FROM `grosmots2` ";
+             Statement st = cnx.createStatement();
+             ResultSet rs = st.executeQuery(req);
+             while (rs.next()) {
+                 grosMots grosmot= new grosMots();
+                 grosmot.setIdmot(rs.getInt(1));
+                 grosmot.setMot(rs.getString(2));
+                                   grosmots.add( grosmot.toStringmot());
+
+             } 
+         } catch (SQLException ex) {
+             Logger.getLogger(grosMotsService.class.getName()).log(Level.SEVERE, null, ex);
+         }
+return  grosmots;
+    }
+    //******************** ajout*********************************************
+             public void ajoutGrosMot(grosMots mot) {
+        try {
+            
+            String req = "INSERT INTO `grosmots2` (`mot`) VALUES (?)";
+            PreparedStatement cat = cnx.prepareStatement(req);
+            cat.setString(1, mot.getMot());
+            cat.executeUpdate();
+            System.out.println("gros mot ajouté");
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+             //********************modification*********************************************
+
+
+
+public void modifierOffre(grosMots mot) {
+            try {
+                
+                String req = "update `grosmots2` set mot=?,where idMOT=?";
+                PreparedStatement pst = cnx.prepareStatement(req);
+                pst.setString(1, mot.getMot());
+                pst.setInt(2, mot.getIdmot());
+                pst.executeUpdate();
+                
+                System.out.println("gros mot modifié  avec succès");
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+}
---
+++ service/offreTravailArchiveService.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import interfaces.offreTravailArchiveInterface;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.control.Alert;
+import models.Categorie;
+import models.allusers;
+import models.offreTravailarchive;
+import util.MaConnexion;
+
+/**
+ *
+ * @author nour2
+ */
+public class offreTravailArchiveService implements offreTravailArchiveInterface {
+
+    Connection cnx = MaConnexion.getInstance().getCnx();
+    allusers studio = new allusers();
+//*******************recuperer l'offre archiver*****************************
+
+    @Override
+    public void recupererOffre(offreTravailarchive o) {
+        try {
+//ajouter la date de recuperation***********************************
+            Date date = new Date();
+            Timestamp sqldate = new java.sql.Timestamp(date.getTime());
+            // inserer loffre une autre fois dans la table offretravail*****************************
+             String req2 = "INSERT INTO `offreTravail2`(`ID_User`,`Nickname`,`titreOffre`, `descriptionOffre`, `categorieOffre`,`dateAjoutOffre`,`typeOffre`,`localisationOffre`,`idCategorie`) VALUES (?,?,?,?,?,?,?,?,?)";
+
+                    PreparedStatement ps = cnx.prepareStatement(req2);
+                    ps.setInt(1, o.getIdStudio());
+                    ps.setString(2, o.getNomStudio());
+                    ps.setString(3, o.getTitreOffre());
+                    ps.setString(4, o.getDescriptionOffre());
+                    ps.setString(5, o.getCategorieOffre().getNomCategorie());
+                    ps.setString(7, o.getTypeOffre());
+                    ps.setString(8, o.getLocalisationOffre());
+                    ps.setInt(9, o.getCategorieOffre().getIdCategorie());
+
+                    ps.setTimestamp(6, sqldate);
+                    ps.executeUpdate();
+            Statement st = cnx.createStatement();
+            //finallement supprimer l'offre de la table d'archive************************
+            String req = "DELETE FROM `offretravailarchive2` WHERE idOffre= " + o.getIdOffre() + "";
+            st.executeUpdate(req);
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText(null);
+            alert.setContentText("votre offre est recuperer");
+
+            alert.showAndWait();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(offreTravailArchiveService.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+    }
+//******************supprimer l'offre definitivement*********************************
+    @Override
+    public void SupprimerDefinitivement(offreTravailarchive o) {
+        try {
+            Statement st = cnx.createStatement();
+            String req = "DELETE FROM `offretravailarchive2` WHERE idOffre= " + o.getIdOffre() + "";
+            st.executeUpdate(req);
+            System.out.println("Offre de travail supprimer avec succès");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText(null);
+            alert.setContentText("offre de travail supprimer avec succès");
+
+            alert.showAndWait();
+        } catch (SQLException ex) {
+            Logger.getLogger(offreTravailArchiveService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    //**********afficher les offres de travail archiver d'utilisateur connecté**************************************
+    @Override
+    public List<offreTravailarchive> fetchOffresarchiverPerIdDate(int id) {
+        List<offreTravailarchive> offresTravail = new ArrayList<>();
+
+        try {
+
+            String req = "SELECT * FROM `offretravailArchive2` WHERE ID_User= \'" + id + "\' ORDER BY dateAjoutOffre DESC";
+
+            Statement st = cnx.createStatement();
+            ResultSet rs = st.executeQuery(req);
+            while (rs.next()) {
+                offreTravailarchive of = new offreTravailarchive();
+
+              Categorie c = new Categorie();
+                of.setIdOffre(rs.getInt("idOffre"));
+                of.setIdStudio(rs.getInt(1));
+                of.setNomStudio(rs.getString(5));
+                of.setTitreOffre(rs.getString(2));
+                of.setDescriptionOffre(rs.getString(3));
+
+                c.setIdCategorie(rs.getInt("idCategorie"));
+                c.setNomCategorie(rs.getString("categorieOffre"));
+                of.setCategorieOffre(c);
+                of.setDateAjoutOffre(rs.getTimestamp(6));
+                of.setTypeOffre(rs.getString(7));
+                of.setLocalisationOffre(rs.getString(8));
+                offresTravail.add(of);
+
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return offresTravail;
+
+    }
+
+    //***********************afficher loffre par son id *************************
+    @Override
+    public offreTravailarchive fetchOffrearchiveParId(int id) {
+        offreTravailarchive of = new offreTravailarchive();
+        try {
+
+            String req = "SELECT * FROM `offretravailarchive2` WHERE idOffre= \'" + id + "\' ";
+
+            Statement st = cnx.createStatement();
+            ResultSet rs = st.executeQuery(req);
+            while (rs.next()) {
+
+                of.setIdOffre(rs.getInt(10));
+                of.setTitreOffre(rs.getString(2));
+                of.setDescriptionOffre(rs.getString(3));
+
+                Categorie c = new Categorie(rs.getInt(9), rs.getString(4));
+                of.setIdStudio(rs.getInt(1));
+                of.setNomStudio(rs.getString(5));
+                of.setCategorieOffre(c);
+                of.setDateAjoutOffre(rs.getTimestamp(6));
+                String categorieinfos = c.toString();
+                of.setTypeOffre(rs.getString(7));
+                of.setLocalisationOffre(rs.getString(8));
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(offreTravailService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return of;
+    }
+}
